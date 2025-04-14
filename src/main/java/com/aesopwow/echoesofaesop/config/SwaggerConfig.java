18b4310cc2740b5ba34762621e9e4808e6562548
hjsong <dev_sklg0602@naver.com>
2025-04-14 11:37:23 +0900

    v1.1.1 Error Solve

--- src/main/java/com/aesopwow/echoesofaesop/config/SwaggerConfig.java
+++ src/main/java/com/aesopwow/echoesofaesop/config/SwaggerConfig.java
@@ -12,10 +12,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI(@Value("${springdoc.swagger-ui.version}") String springdocVersion) {
+    public OpenAPI openAPI(@Value("1.1.1") String springdocVersion) {
         Info info = new Info()
                 .title("AesopWow 프로젝트")
-                .version("1.1.0")
                 .description("AesopWow 프로젝트는 고민을 나누고, 비슷한 경험을 가진 사람들로부터 조언과 공감을 얻음으로써 심리적 안정감을 제공.<br>"
                         + "이솝우화가 정치적, 사회적 메시지를 전달했던 것처럼, 익명 게시판은 현대 사회에서 사람들이 자유롭게 의견을 나눌 수 있는 중요한 역할을 수행.<br>"
                         + "이솝우화의 교훈적 특성을 현대적인 방식으로 재해석하여 새로운 형태의 지혜 공유를 실현.<br>"
